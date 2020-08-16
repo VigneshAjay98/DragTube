d047f942e718f7bee3e91a2b845e8288bafecde1
Vignesh Ajay <vigneshajay13111998@gmail.com>
2020-08-17 00:01:31 +0530

    Updates

---
+++ precache-manifest.31bc5efb189eff17ee71b10c44c4c9ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "595bed713505c24cda9e566d192fdf23",
+    "url": "/DragTube/index.html"
+  },
+  {
+    "revision": "d5b6dcf9debf5534b1e0",
+    "url": "/DragTube/static/css/main.1de16a8a.chunk.css"
+  },
+  {
+    "revision": "ba562794be4a55816747",
+    "url": "/DragTube/static/js/2.cb1c89ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DragTube/static/js/2.cb1c89ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5b6dcf9debf5534b1e0",
+    "url": "/DragTube/static/js/main.ac562a84.chunk.js"
+  },
+  {
+    "revision": "5133eb5aea8590f390e0",
+    "url": "/DragTube/static/js/runtime-main.295e1e3b.js"
+  }
+]);
